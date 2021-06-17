24af40ab844e2f66a250376c60acd9c7177d9dfe
Tristan Tinning <tristan_tinning@hotmail.com>
2021-06-17 14:04:52 +0100

    MVP complete

--- js/app.js
+++ js/app.js
@@ -1,3 +1,21 @@
 document.addEventListener('DOMContentLoaded', () => {
-  console.log('JavaScript loaded');
-})
+  const form = document.querySelector('#new-item-form');
+  form.addEventListener('submit', handleNewForm);  
+});
+
+const handleNewForm = function(event) {
+  event.preventDefault();
+  const readingList = document.querySelector('#reading-list');
+  const readingListItem = document.createElement('li');
+  readingList.appendChild(readingListItem);
+  const title = document.createElement('h2');
+  title.textContent = event.target.title.value;
+  readingListItem.appendChild(title);
+  const author = document.createElement('h3');
+  author.textContent = event.target.author.value;
+  readingListItem.appendChild(author)
+  const category = document.createElement('h4');
+  category.textContent = event.target.category.value;
+  readingListItem.appendChild(category);
+  this.reset();
+};
